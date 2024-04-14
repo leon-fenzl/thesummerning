64cb1e5f55d455825d76c808bdde47293c6d21a7
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-04-13 22:24:04 -0300

    Targeting System V1

--- Thesummerning/scripts/Leon/CamCtrlr.gd
+++ Thesummerning/scripts/Leon/CamCtrlr.gd
@@ -1,29 +1,20 @@
 extends SpringArm3D
 
-@export var displace_Height:float
-@export var minLength:float
-@export var maxLength:float
-@export var pitch := Vector2(-80.0,80.0)
-@export var yaw := Vector2(0.0,360.0)
-@onready var camInputs := Vector2.ZERO
+@onready var camera := $GameCamera
 @export var playerRef : NodePath
 @onready var player = get_node(playerRef)
-
-func _ready():
-	set_as_top_level(true)
-func _input(event: InputEvent) -> void:
-	pass
-	#if event is InputEventMouseMotion:
-		#rotation.x -= event.relative.y*parent.mouseSpeed*get_physics_process_delta_time()
-		#rotation.x = clamp(rotation.x,deg_to_rad(pitch.x),deg_to_rad(pitch.y))
-		#rotation.y -= event.relative.x*parent.mouseSpeed*get_physics_process_delta_time()
-		#rotation.y = wrapf(rotation.y,deg_to_rad(yaw.x),deg_to_rad(yaw.y))
-func _physics_process(delta: float) -> void:
+@onready var world
+@onready var mousepos
+@onready var rayOrigin 
+@onready var rayEnd
+@onready var query 
+func _physics_process(delta):
 	position = lerp(position,player.global_position,10*delta)
-func Controller_Inputs()-> void:
-	camInputs = Input.get_vector("camLeft","camRight","camUp","camDown").normalized()
-func Ctrlr_Pitch_Yaw(DELTA:float)-> void:
-	rotation.x -= camInputs.y*player.controllerSpeed*DELTA
-	rotation.x = clamp(rotation.x,deg_to_rad(pitch.x),deg_to_rad(pitch.y))
-	rotation.y -= camInputs.x*player.controllerSpeed*DELTA
-	rotation.y = wrapf(rotation.y,deg_to_rad(yaw.x),deg_to_rad(yaw.y))
+	DrawRaycast()
+	player.rayhitInfo = query 
+func DrawRaycast():
+	world = get_world_3d().direct_space_state
+	mousepos = get_viewport().get_mouse_position()
+	rayOrigin = camera.project_ray_origin(mousepos)
+	rayEnd = rayOrigin + camera.project_position(mousepos,1000)
+	query = world.intersect_ray(PhysicsRayQueryParameters3D.create(rayOrigin, rayEnd))
--- Thesummerning/scripts/Leon/GameManager.gd
+++ Thesummerning/scripts/Leon/GameManager.gd
@@ -3,7 +3,6 @@ extends Node
 #func _input(event):
 	#Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
-
 func _physics_process(delta):
 	if Input.is_action_just_pressed("exit"):
 		get_tree().paused = !get_tree().paused
@@ -11,5 +10,4 @@ func _physics_process(delta):
 func ShowMouseCursor():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 func Unpause_Screen_Visbility():
-	get_tree().paused = !get_tree().paused
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
