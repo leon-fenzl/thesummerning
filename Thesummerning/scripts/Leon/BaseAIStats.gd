8d65a8f5e9963dc3555bc43339ce97de1a8f0239
Blaze <32179683+leon-fenzl@users.noreply.github.com>
2024-04-15 16:19:28 -0300

    Update Cenário Final

--- Thesummerning/scripts/Leon/BaseAIStats.gd
+++ Thesummerning/scripts/Leon/BaseAIStats.gd
@@ -1,39 +1,48 @@
 extends CharacterBody3D
 class_name BaseAI
-enum STATES{WALK,ATK,DEAD}
+
+enum AI_TYPES {GUARD,MOB}
+enum MOVE_TYPES{WALK,ATK,DEAD}
 
 @export var SPEED := 500.0
 @export var LIFE := 0
+
+@export var baseType = AI_TYPES.GUARD
+@onready var moveState = MOVE_TYPES.WALK
+
 @onready var lifeCtrl := $CompLife
 @onready var damageCtrl := $CompDamage
 @onready var agent := $NavigationAgent3D
-@onready var state = STATES.WALK
 
-@onready var current_location := Vector3.ZERO
 @onready var gravityVector = ProjectSettings.get_setting("physics/3d/default_gravity")
+@onready var current_location := Vector3.ZERO
+@onready var next_location := Vector3.ZERO
+@onready var new_direction := Vector3.ZERO
+
+@onready var target : Node3D
 func _ready():
 	LIFE = lifeCtrl.life
-
 func _physics_process(delta):
 	GravitySystem(delta)
-	
-	match state:
-		STATES.WALK:
-		if(!arrived):
-			var current_location = global_transform.origin
-			var next_location = nav_agent.get_next_path_position()
-			var new_velocity = (next_location - current_location).normalized() * SPEED
+	match moveState:
+		MOVE_TYPES.WALK:
+			current_location = global_transform.origin
+			next_location = agent.get_next_path_position()
+			new_direction = (next_location - current_location).normalized() * SPEED * delta
 			WALK()
-		STATES.ATK:
+		MOVE_TYPES.ATK:
 			ATTACK()
 func WALK():
-	
-		velocity = velocity.move_toward(new_velocity, .25)
+		velocity = new_direction
 		move_and_slide()
+func Update_target_location(location:Vector3):
+	agent.set_target_position(location)
 func ATTACK():
 	pass
 func GravitySystem(DELTA:float):
 	if !is_on_floor():
 		gravityVector *= 100 * DELTA
 	else: gravityVector = -get_floor_normal()
-func Calcula
+func _on_timer_timeout():
+	print(target.global_position)
+	Update_target_location(target.global_position)
--- Thesummerning/scripts/Leon/MobSpawner.gd
+++ Thesummerning/scripts/Leon/MobSpawner.gd
@@ -3,6 +3,8 @@ extends Node3D
 @export var mobScenes:Array[PackedScene]
 @onready var index := 0
 @onready var mobNode : Node3D
+@export var playerRef:NodePath
+@onready var player := get_node(playerRef)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	SpawnRandomAI()
@@ -10,9 +12,10 @@ func _ready():
 func _process(delta):
 	pass
 func SpawnRandomAI():
-	load(mobScenes[index].get_path())
 	mobNode = mobScenes[index].instantiate()
 	add_child(mobNode)
+	mobNode.target = player
+	print(mobNode.target)
 	mobNode.set_as_top_level(true)
 	if index <= mobScenes.size()-1:index = 0
 	else: index += 1
